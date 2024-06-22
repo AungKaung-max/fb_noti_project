52de2b912a281c98ca077283636faf26167448f4
AungKaung <aungkaungmyathtun@gmail.com>
2024-06-21 18:55:03 -0700

    updated for test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
-const app = require("./app");
-require("dotenv").config();
 const config = require("./src/config/config");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongodbConnection = require("./src/db/index");
+const postRoutes = require("./src/routes/postRoutes");
+const userRoutes = require("./src/routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+mongodbConnection();
+
+app.use("/api", postRoutes);
+app.use("/api", userRoutes);
 
 const PORT = config.port || 4000;
 
